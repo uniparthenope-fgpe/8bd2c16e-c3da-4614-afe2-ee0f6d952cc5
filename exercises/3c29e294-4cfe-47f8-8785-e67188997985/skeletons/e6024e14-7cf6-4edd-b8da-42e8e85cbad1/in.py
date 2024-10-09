7215c99c0cd8ab5a989060f0fa1f0f11ba46e676
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2024-10-09 09:01:34 +0200

    created file exercises/3c29e294-4cfe-47f8-8785-e67188997985/skeletons/e6024e14-7cf6-4edd-b8da-42e8e85cbad1/in.py

---
+++ exercises/3c29e294-4cfe-47f8-8785-e67188997985/skeletons/e6024e14-7cf6-4edd-b8da-42e8e85cbad1/in.py
@@ -0,0 +1,6 @@
+def count_ingredients(ingredients):
+    # Fill in the gaps to count ingredients
+    return count
+
+potion_ingredients = ['Eye of Newt', 'Dragon Liver', 'Wormwood']
+print(count_ingredients(potion_ingredients))
