e7a3d5c5bf8037e2f0df05c33ec1daca3dff2adf
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2024-10-09 16:06:59 +0200

    created file exercises/755e4cd4-f1f1-4a91-8231-3dcbaa35611a/solutions/8d467e1e-f9c6-4f85-9dd2-0f0594c744a2/sol.py

---
+++ exercises/755e4cd4-f1f1-4a91-8231-3dcbaa35611a/solutions/8d467e1e-f9c6-4f85-9dd2-0f0594c744a2/sol.py
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string droids[3] = {'R2-D2', 'C-3PO', 'BB-8'};
+    for (int i = 0; i < 3; i++) {
+        cout << droids[i] << endl;
+    }
+    return 0;
+}
