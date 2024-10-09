4c1a841f7450f8806f57eea13d2a5b6ddfe99bf9
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2024-10-09 12:42:11 +0200

    created file exercises/755e4cd4-f1f1-4a91-8231-3dcbaa35611a/skeletons/b98a6636-caf4-4c4b-9112-126a6fcdf8ee/in.py

---
+++ exercises/755e4cd4-f1f1-4a91-8231-3dcbaa35611a/skeletons/b98a6636-caf4-4c4b-9112-126a6fcdf8ee/in.py
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string droids[3] = {'R2-D2', 'C-3PO', 'BB-8'};
+    // Complete the loop to print the droid names
+    for (int i = 0; i < 3; i++) {
+        cout << droids[i] << endl;
+    }
+    return 0;
+}
