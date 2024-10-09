459cd7d89b30b23d4237fd34f04cff9c9392e465
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2024-10-09 09:23:46 +0200

    created file exercises/e94365e3-2918-446e-9167-9a02636f1a7f/skeletons/9f27270e-d07a-42be-910e-e56680ac1def/in.py

---
+++ exercises/e94365e3-2918-446e-9167-9a02636f1a7f/skeletons/9f27270e-d07a-42be-910e-e56680ac1def/in.py
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class Starship {
+    // Define attributes and methods here
+};
+
+int main() {
+    Starship falcon;
+    // Set attributes and call the display method
+    return 0;
+}
