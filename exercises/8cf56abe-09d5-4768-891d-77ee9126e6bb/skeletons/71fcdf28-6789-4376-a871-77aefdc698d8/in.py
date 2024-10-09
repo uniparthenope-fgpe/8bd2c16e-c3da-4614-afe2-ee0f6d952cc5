ba569b9021ca22d29e9f977967c7d11e9512912e
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2024-10-09 09:04:46 +0200

    created file exercises/8cf56abe-09d5-4768-891d-77ee9126e6bb/skeletons/71fcdf28-6789-4376-a871-77aefdc698d8/in.py

---
+++ exercises/8cf56abe-09d5-4768-891d-77ee9126e6bb/skeletons/71fcdf28-6789-4376-a871-77aefdc698d8/in.py
@@ -0,0 +1,8 @@
+class MagicalCreature:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+    # Add methods here
+
+# Create an instance of MagicalCreature
+creature = MagicalCreature('Fawkes', 'Phoenix')
