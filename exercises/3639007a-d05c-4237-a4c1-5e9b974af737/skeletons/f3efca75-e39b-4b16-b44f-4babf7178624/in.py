39c320475eaeb065c13728165d69352b6e589446
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2024-10-09 09:01:35 +0200

    created file exercises/3639007a-d05c-4237-a4c1-5e9b974af737/skeletons/f3efca75-e39b-4b16-b44f-4babf7178624/in.py

---
+++ exercises/3639007a-d05c-4237-a4c1-5e9b974af737/skeletons/f3efca75-e39b-4b16-b44f-4babf7178624/in.py
@@ -0,0 +1,7 @@
+def sort_student(traits):
+    if 'bravery' in traits:
+        return 'Gryffindor'
+    # Complete the rest of the conditions
+
+student_traits = ['bravery', 'loyalty']
+print(sort_student(student_traits))
