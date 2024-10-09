cd5579bea2a923d7dae381811412a027a80920fe
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2024-10-09 09:17:25 +0200

    created file exercises/bca456a2-71d2-4cf7-8af8-a0d371fa4b63/skeletons/ef2dbe49-81a2-42ed-b8c5-ac1484179e9a/in.py

---
+++ exercises/bca456a2-71d2-4cf7-8af8-a0d371fa4b63/skeletons/ef2dbe49-81a2-42ed-b8c5-ac1484179e9a/in.py
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    // Fill in the gaps to complete the greeting
+    cout << 'Hello, ' << name << '!' << endl;
+    return 0;
+}
