c5bd888c46351652a9850c26ed86c0c598ff0eb2
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2024-10-09 09:17:23 +0200

    created file exercises/d6ef73d3-6602-41d5-9d12-235e00a3d06f/skeletons/82de2de0-23cc-4829-a118-a3ad3a181429/in.py

---
+++ exercises/d6ef73d3-6602-41d5-9d12-235e00a3d06f/skeletons/82de2de0-23cc-4829-a118-a3ad3a181429/in.py
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void sortSithNames(string names[], int size) {
+    // Implement the sorting algorithm here
+}
+
+int main() {
+    string siths[] = {'Darth Vader', 'Darth Maul', 'Darth Sidious'};
+    sortSithNames(siths, 3);
+    // Print sorted names
+    return 0;
+}
