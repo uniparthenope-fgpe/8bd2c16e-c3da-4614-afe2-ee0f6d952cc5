e2989fefac2e68dcb1c525950548e0b1f39c7535
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2024-10-09 09:01:58 +0200

    created file exercises/3c29e294-4cfe-47f8-8785-e67188997985/solutions/8b36ba53-d886-4067-882c-d0d270006924/sol.py

---
+++ exercises/3c29e294-4cfe-47f8-8785-e67188997985/solutions/8b36ba53-d886-4067-882c-d0d270006924/sol.py
@@ -0,0 +1,6 @@
+def count_ingredients(ingredients):
+    count = len(ingredients)
+    return count
+
+potion_ingredients = ['Eye of Newt', 'Dragon Liver', 'Wormwood']
+print(count_ingredients(potion_ingredients))
