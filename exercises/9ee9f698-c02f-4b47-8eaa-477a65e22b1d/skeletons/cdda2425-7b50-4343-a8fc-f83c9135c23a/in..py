502d01d9c1f9158b6c6922924ca7b0db5a3112c8
Ciro Giuseppe De Vita <cirogiuseppe.devita001@studenti.uniparthenope.it>
2025-06-06 19:01:53 +0200

    created file exercises/9ee9f698-c02f-4b47-8eaa-477a65e22b1d/skeletons/cdda2425-7b50-4343-a8fc-f83c9135c23a/in..py

---
+++ exercises/9ee9f698-c02f-4b47-8eaa-477a65e22b1d/skeletons/cdda2425-7b50-4343-a8fc-f83c9135c23a/in..py
@@ -0,0 +1,9 @@
+class Wizard:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+    
+    # Add methods for casting spells and taking damage
+
+harry = Wizard('Harry')
+# Simulate a duel here
